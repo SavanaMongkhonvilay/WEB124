937c7b5cbb7cd01ab7a58d88f93535df88f2db02
Savana Mongkhonvilay <savanamongkhonvilay@gmail.com>
2025-06-10 20:55:00 -0500

    Create project2.js

---
+++ ch2/project2.js
@@ -0,0 +1,9 @@
+// Savana Mongkhonvilay 6/10/25
+"use strict"; 
+
+var myName; 
+var para1; 
+
+let myName = "Savana Mongkhonvilay"; 
+let para1 = "document.getElementById("p1");
+para1.textContent = myName;
