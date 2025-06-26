69cf9aca89c8d35027744b286bfed792a2a5dc27
Savana Mongkhonvilay <savanamongkhonvilay@gmail.com>
2025-06-26 11:59:32 -0500

    Create javascript.js

---
+++ Chapter3Project/javascript.js
@@ -0,0 +1,23 @@
+// Savana Mongkhonvilay 6/26/25
+
+"use strict"; 
+
+//variables 
+let fullName = "Savana Mongkhonvilay";
+console.log(fullName); 
+let desiredSalary = 80000; 
+console.log(desiredSalary); 
+let veteran = false; 
+console.log(veteran); 
+let friends = ["Aiden", "Christina", "Lisa"]; 
+console.log(friends); 
+let friendsDesired = [75000 ,90000, 60000]; 
+console.log(friendsDesired); 
+
+let anotherFriend = {
+    firstName: "Kenzie",
+    lastName: "Iden",
+    friendDesired: 100000
+}; 
+
+console.log(anotherFriend); 
